182fc291a9bc0cc61a2704eb90a0e9c9900f68c1
thevictoriax <viktoriia.makukh@lnu.edu.ua>
2023-02-28 10:49:30 +0200

    Add project files.

---
+++ ОЧ2/Zalik.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ОЧ2
+{
+    public class Zalik
+    {
+        private string subject;
+        private string teacher;
+        private double grade;
+
+        public Zalik(string _subject, string _teacher, double _grade) 
+        {
+            grade = _grade;
+            subject = _subject;
+            teacher = _teacher;
+        }
+
+    }
+}
