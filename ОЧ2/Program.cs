2301674c8814a92182e85a60a3f26959ea1690c7
Vorobienko Bohdan <bogdanvorobienko@gmail.com>
2023-03-03 16:50:26 +0200

    made derived class, added IComparable implementation to Zalik, rewrote fields to properties

--- ОЧ2/Program.cs
+++ ОЧ2/Program.cs
@@ -3,5 +3,8 @@ using System.Runtime.InteropServices;
 using ОЧ2;
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
-Zalik a = new Zalik("OP", "Ярошко", 51);
-Console.WriteLine(a.ToString());
+Zalik a = new Zalik("OP", "Ярошко", 56);
+Zalik b = new Zalik("OP", "Ярошко", 56);
+Exam e = new Exam("MS", "Teacher", "Assistant", 78);
+
+Console.WriteLine(e);
